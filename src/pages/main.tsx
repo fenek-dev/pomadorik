d2ad2b078cbddace79598c173e631534ceed9cb0
MaksFenek <makstreonin@gmail.com>
2023-12-17 11:44:23 +0300

    project setup

---
+++ src/pages/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MainPage = () => {
+  return (
+    <div className="h-screen w-screen bg-base-100">
+      <button className="btn">Button</button>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "var(--primary-color)",
+          secondary: "var(--secondary-color)",
+          "base-100": "var(--base-color)",
+          success: "var(--success-color)",
+          error: "var(--error-color)",
+        },
+      },
+    ],
   },
   plugins: [daisyui],
 };
